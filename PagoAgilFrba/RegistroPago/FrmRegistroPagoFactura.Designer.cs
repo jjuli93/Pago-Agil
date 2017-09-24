c410c18cdde9a881f3d752ce3a22f759f9c903aa
jcorvalan93 <jcorvalan93@hotmail.com>
2017-09-24 16:40:27 -0300

    Comienzo de la vista de pago factura
    FrmRegistroPagoFactura

---
+++ PagoAgilFrba/RegistroPago/FrmRegistroPagoFactura.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace PagoAgilFrba.RegistroPago
+{
+    partial class FrmRegistroPagoFactura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRegistroPagoFactura));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tituloLb = new System.Windows.Forms.Label();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.abmPnl = new System.Windows.Forms.Panel();
+            this.volverBtn = new System.Windows.Forms.Button();
+            this.crearBtn = new System.Windows.Forms.Button();
+            this.cerrarSesionHl = new System.Windows.Forms.LinkLabel();
+            this.exitBtn = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.obligatorio3 = new System.Windows.Forms.Label();
+            this.obligatorio1 = new System.Windows.Forms.Label();
+            this.obligatorio2 = new System.Windows.Forms.Label();
+            this.obligatoriosLbl = new System.Windows.Forms.Label();
+            this.habilitadoChk = new System.Windows.Forms.CheckBox();
+            this.cuitTb = new System.Windows.Forms.TextBox();
+            this.direccionTb = new System.Windows.Forms.TextBox();
+            this.nombreTb = new System.Windows.Forms.TextBox();
+            this.cuitLb = new System.Windows.Forms.Label();
+            this.dirLb = new System.Windows.Forms.Label();
+            this.nombreLb = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.abmPnl.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.tituloLb);
+            this.panel1.Controls.Add(this.logoPictureBox);
+            this.panel1.Controls.Add(this.abmPnl);
+            this.panel1.Controls.Add(this.cerrarSesionHl);
+            this.panel1.Controls.Add(this.exitBtn);
+            this.panel1.Location = new System.Drawing.Point(-3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 65);
+            this.panel1.TabIndex = 10;
+            // 
+            // tituloLb
+            // 
+            this.tituloLb.AutoSize = true;
+            this.tituloLb.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tituloLb.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.tituloLb.Location = new System.Drawing.Point(74, 9);
+            this.tituloLb.Name = "tituloLb";
+            this.tituloLb.Size = new System.Drawing.Size(157, 18);
+            this.tituloLb.TabIndex = 10;
+            this.tituloLb.Text = "Cobro de Facturas";
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
+            this.logoPictureBox.Location = new System.Drawing.Point(1, 1);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(61, 63);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoPictureBox.TabIndex = 9;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // abmPnl
+            // 
+            this.abmPnl.Controls.Add(this.button1);
+            this.abmPnl.Controls.Add(this.volverBtn);
+            this.abmPnl.Controls.Add(this.crearBtn);
+            this.abmPnl.Location = new System.Drawing.Point(71, 30);
+            this.abmPnl.Name = "abmPnl";
+            this.abmPnl.Size = new System.Drawing.Size(406, 29);
+            this.abmPnl.TabIndex = 4;
+            // 
+            // volverBtn
+            // 
+            this.volverBtn.Location = new System.Drawing.Point(325, 3);
+            this.volverBtn.Name = "volverBtn";
+            this.volverBtn.Size = new System.Drawing.Size(75, 23);
+            this.volverBtn.TabIndex = 4;
+            this.volverBtn.Text = "Volver";
+            this.volverBtn.UseVisualStyleBackColor = true;
+            // 
+            // crearBtn
+            // 
+            this.crearBtn.Location = new System.Drawing.Point(3, 3);
+            this.crearBtn.Name = "crearBtn";
+            this.crearBtn.Size = new System.Drawing.Size(88, 23);
+            this.crearBtn.TabIndex = 0;
+            this.crearBtn.Text = "Cargar factura";
+            this.crearBtn.UseVisualStyleBackColor = true;
+            // 
+            // cerrarSesionHl
+            // 
+            this.cerrarSesionHl.AutoSize = true;
+            this.cerrarSesionHl.Location = new System.Drawing.Point(491, 41);
+            this.cerrarSesionHl.Name = "cerrarSesionHl";
+            this.cerrarSesionHl.Size = new System.Drawing.Size(67, 13);
+            this.cerrarSesionHl.TabIndex = 8;
+            this.cerrarSesionHl.TabStop = true;
+            this.cerrarSesionHl.Text = "cerrar sesión";
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.AutoSize = true;
+            this.exitBtn.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.exitBtn.LinkColor = System.Drawing.Color.LightGray;
+            this.exitBtn.Location = new System.Drawing.Point(532, 9);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(16, 13);
+            this.exitBtn.TabIndex = 7;
+            this.exitBtn.TabStop = true;
+            this.exitBtn.Text = "X";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Generar Pago";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.obligatorio3);
+            this.groupBox1.Controls.Add(this.obligatorio1);
+            this.groupBox1.Controls.Add(this.obligatorio2);
+            this.groupBox1.Controls.Add(this.obligatoriosLbl);
+            this.groupBox1.Controls.Add(this.habilitadoChk);
+            this.groupBox1.Controls.Add(this.cuitTb);
+            this.groupBox1.Controls.Add(this.direccionTb);
+            this.groupBox1.Controls.Add(this.nombreTb);
+            this.groupBox1.Controls.Add(this.cuitLb);
+            this.groupBox1.Controls.Add(this.dirLb);
+            this.groupBox1.Controls.Add(this.nombreLb);
+            this.groupBox1.Location = new System.Drawing.Point(10, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(537, 165);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de la factura";
+            // 
+            // obligatorio3
+            // 
+            this.obligatorio3.AutoSize = true;
+            this.obligatorio3.BackColor = System.Drawing.Color.Transparent;
+            this.obligatorio3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.obligatorio3.ForeColor = System.Drawing.Color.DarkRed;
+            this.obligatorio3.Location = new System.Drawing.Point(499, 36);
+            this.obligatorio3.Name = "obligatorio3";
+            this.obligatorio3.Size = new System.Drawing.Size(20, 25);
+            this.obligatorio3.TabIndex = 18;
+            this.obligatorio3.Text = "*";
+            // 
+            // obligatorio1
+            // 
+            this.obligatorio1.AutoSize = true;
+            this.obligatorio1.BackColor = System.Drawing.Color.Transparent;
+            this.obligatorio1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.obligatorio1.ForeColor = System.Drawing.Color.DarkRed;
+            this.obligatorio1.Location = new System.Drawing.Point(246, 36);
+            this.obligatorio1.Name = "obligatorio1";
+            this.obligatorio1.Size = new System.Drawing.Size(20, 25);
+            this.obligatorio1.TabIndex = 17;
+            this.obligatorio1.Text = "*";
+            // 
+            // obligatorio2
+            // 
+            this.obligatorio2.AutoSize = true;
+            this.obligatorio2.BackColor = System.Drawing.Color.Transparent;
+            this.obligatorio2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.obligatorio2.ForeColor = System.Drawing.Color.DarkRed;
+            this.obligatorio2.Location = new System.Drawing.Point(246, 67);
+            this.obligatorio2.Name = "obligatorio2";
+            this.obligatorio2.Size = new System.Drawing.Size(20, 25);
+            this.obligatorio2.TabIndex = 16;
+            this.obligatorio2.Text = "*";
+            // 
+            // obligatoriosLbl
+            // 
+            this.obligatoriosLbl.AutoSize = true;
+            this.obligatoriosLbl.ForeColor = System.Drawing.Color.DarkRed;
+            this.obligatoriosLbl.Location = new System.Drawing.Point(421, 81);
+            this.obligatoriosLbl.Name = "obligatoriosLbl";
+            this.obligatoriosLbl.Size = new System.Drawing.Size(98, 13);
+            this.obligatoriosLbl.TabIndex = 15;
+            this.obligatoriosLbl.Text = "* Campo obligatorio";
+            // 
+            // habilitadoChk
+            // 
+            this.habilitadoChk.AutoSize = true;
+            this.habilitadoChk.Location = new System.Drawing.Point(289, 77);
+            this.habilitadoChk.Name = "habilitadoChk";
+            this.habilitadoChk.Size = new System.Drawing.Size(73, 17);
+            this.habilitadoChk.TabIndex = 8;
+            this.habilitadoChk.Text = "Habilitado";
+            this.habilitadoChk.UseVisualStyleBackColor = true;
+            // 
+            // cuitTb
+            // 
+            this.cuitTb.Location = new System.Drawing.Point(329, 36);
+            this.cuitTb.Name = "cuitTb";
+            this.cuitTb.Size = new System.Drawing.Size(164, 20);
+            this.cuitTb.TabIndex = 6;
+            // 
+            // direccionTb
+            // 
+            this.direccionTb.Location = new System.Drawing.Point(86, 67);
+            this.direccionTb.Name = "direccionTb";
+            this.direccionTb.Size = new System.Drawing.Size(154, 20);
+            this.direccionTb.TabIndex = 5;
+            // 
+            // nombreTb
+            // 
+            this.nombreTb.Location = new System.Drawing.Point(76, 36);
+            this.nombreTb.Name = "nombreTb";
+            this.nombreTb.Size = new System.Drawing.Size(164, 20);
+            this.nombreTb.TabIndex = 4;
+            // 
+            // cuitLb
+            // 
+            this.cuitLb.AutoSize = true;
+            this.cuitLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuitLb.Location = new System.Drawing.Point(286, 39);
+            this.cuitLb.Name = "cuitLb";
+            this.cuitLb.Size = new System.Drawing.Size(35, 13);
+            this.cuitLb.TabIndex = 2;
+            this.cuitLb.Text = "CUIT:";
+            // 
+            // dirLb
+            // 
+            this.dirLb.AutoSize = true;
+            this.dirLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dirLb.Location = new System.Drawing.Point(20, 70);
+            this.dirLb.Name = "dirLb";
+            this.dirLb.Size = new System.Drawing.Size(55, 13);
+            this.dirLb.TabIndex = 1;
+            this.dirLb.Text = "Dirección:";
+            // 
+            // nombreLb
+            // 
+            this.nombreLb.AutoSize = true;
+            this.nombreLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombreLb.Location = new System.Drawing.Point(20, 39);
+            this.nombreLb.Name = "nombreLb";
+            this.nombreLb.Size = new System.Drawing.Size(47, 13);
+            this.nombreLb.TabIndex = 0;
+            this.nombreLb.Text = "Nombre:";
+            // 
+            // FrmRegistroPagoFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 488);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmRegistroPagoFactura";
+            this.Text = "FrmRegistroPagoFactura";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.abmPnl.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label tituloLb;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Panel abmPnl;
+        private System.Windows.Forms.Button volverBtn;
+        private System.Windows.Forms.Button crearBtn;
+        private System.Windows.Forms.LinkLabel cerrarSesionHl;
+        private System.Windows.Forms.LinkLabel exitBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label obligatorio3;
+        private System.Windows.Forms.Label obligatorio1;
+        private System.Windows.Forms.Label obligatorio2;
+        private System.Windows.Forms.Label obligatoriosLbl;
+        private System.Windows.Forms.CheckBox habilitadoChk;
+        private System.Windows.Forms.TextBox cuitTb;
+        private System.Windows.Forms.TextBox direccionTb;
+        private System.Windows.Forms.TextBox nombreTb;
+        private System.Windows.Forms.Label cuitLb;
+        private System.Windows.Forms.Label dirLb;
+        private System.Windows.Forms.Label nombreLb;
+    }
+}
--- PagoAgilFrba/RegistroPago/FrmRegistroPagoFactura.cs
+++ PagoAgilFrba/RegistroPago/FrmRegistroPagoFactura.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PagoAgilFrba.RegistroPago
 {
-    public partial class Form1 : Form
+    public partial class FrmRegistroPagoFactura : Form
     {
-        public Form1()
+        public FrmRegistroPagoFactura()
         {
             InitializeComponent();
         }
