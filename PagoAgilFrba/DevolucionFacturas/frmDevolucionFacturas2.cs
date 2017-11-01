c4d189c5f6d57c6b31897eb64b3eb20d3d7a32d8
Julián Ignacio Alvarez <alvarezjulianignacio@gmail.com>
2017-11-01 19:52:28 -0300

    diseño devolucion y pago

--- PagoAgilFrba/DevolucionFacturas/frmDevolucionFacturas2.cs
+++ PagoAgilFrba/DevolucionFacturas/frmDevolucionFacturas2.cs
@@ -16,5 +16,10 @@ namespace PagoAgilFrba.DevolucionFacturas
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
